167ad9f08ba883cc591275a1a26bfb63d442d38a
Glitch (glitch-hello-node) <none>
2024-12-20 10:21:50 +0000

    🍜💓 Checkpoint ./index.js:72855240/8 ./public/index.html:72855240/17008

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require("cors"); // Import the cors middleware
 const path = require("path"); // Import the path module
 
 const app = express();
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors()); // Enable CORS
