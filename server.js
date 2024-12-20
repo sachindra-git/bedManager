d5cf081cfa8fe9e718972f4d84d06c80f87e6e08
Glitch (glitch-hello-node) <none>
2024-12-20 09:25:00 +0000

    ⛲️🚖 Checkpoint ./server.js:72855240/803

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
+const express = require("express");
+const router = express.Router();
 
 // MongoDB connection
 mongoose.connect(
-  "mongodb+srv://supunadmin:T394Gh54Vd9pO121@cluster01.leeclw9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster01",
+  "mongodb+srv://chathurangawijayarathneeb:HNesZUAawQsCOkl3@cluster01.nzv8k.mongodb.net/?retryWrites=true&w=majority&appName=bedManager",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -13,7 +15,7 @@ const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 db.once("open", () => {
-  console.log("Connected to MongoDB");
+  console.log("Connected to MongoDBaaa");
 });
 
 
@@ -29,6 +31,16 @@ const userSchema = new mongoose.Schema({
 
 // Create a model
 const icuList = mongoose.model('icuList', userSchema);
+
+router.get("/", async (req, res) => {
+  try {
+    const iculist = await icuList.find();
+    res.json(iculist);
+    console.log(res.json(iculist), '');
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 // Fetch and print data
 async function fetchData() {
