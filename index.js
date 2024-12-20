749bd54acff4be87639e5cb8f03aa60aba1aa1f5
Glitch (glitch-hello-node) <none>
2024-12-20 10:44:04 +0000

    🐯🍞 Checkpoint ./index.js:72855240/5272

--- index.js
+++ index.js
@@ -1,46 +1,20 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const cors = require("cors"); // Import the cors middleware
-const path = require("path"); // Import the path module
+const { MongoClient } = require('mongodb');
 
-const app = express();
-const port = process.env.PORT || 5000;
+const uri = "<Your Connection String>";
+const client = new MongoClient(uri);
 
-// Middleware
-app.use(cors()); // Enable CORS
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+async function run() {
+  try {
+    await client.connect();
+    const db = client.db('sample_mflix');
+    const collection = db.collection('movies');
 
-// Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, "public")));
-
-// Routes
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html")); // Serve index.html as the default page
-});
-
-// Component Routes
-const componentRoutes = require("./routes/componentRoutes");
-app.use("/components", componentRoutes);
-
-// MongoDB connection
-mongoose.connect(
-  "mongodb+srv://chathurangawijayarathneeb:HNesZUAawQsCOkl3@cluster01.nzv8k.mongodb.net/?retryWrites=true&w=majority&appName=bedManager",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+    // Find the first document in the collection
+    const first = await collection.findOne();
+    console.log(first);
+  } finally {
+    // Close the database connection when finished or an error occurs
+    await client.close();
   }
-);
-
-const db = mongoose.connection;
-
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
-db.once("open", () => {
-  console.log("Connected to MongoDB");
-});
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+}
+run().catch(console.error);
