d3621b2ecec39dd2236f95f9c9d66ffd53ca7724
Glitch (glitch-hello-node) <none>
2024-12-20 09:13:23 +0000

    🎎👗 Checkpoint ./server.js:72855240/1311

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const mongoose = require('mongoose');
 
-// MongoDB connection URI
-const uri = 'mongodb+srv://chathurangawijayarathneeb:HNesZUAawQsCOkl3@cluster01.nzv8k.mongodb.net/?retryWrites=true&w=majority&appName=bedManager';
+// MongoDB connection
+mongoose.connect(
+  "mongodb+srv://supunadmin:T394Gh54Vd9pO121@cluster01.leeclw9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster01",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
-// Connect to MongoDB
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('MongoDB connection error:', err));
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", () => {
+  console.log("Connected to MongoDB");
+});
+
+
 
 // Define a schema
 const userSchema = new mongoose.Schema({
@@ -24,7 +34,7 @@ const icuList = mongoose.model('icuList', userSchema);
 async function fetchData() {
   try {
     const icudata = await icuList.find(); // Fetch all documents from the "users" collection
-    console.log('Data from the database:', icudata);
+    console.log('Data from the database aaaaaaaaaaaa:', icudata);
   } catch (err) {
     console.error('Error fetching data:', err);
   } finally {
