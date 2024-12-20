903ef45da960df4e748406f27c2d1fabb34eee02
Glitch (glitch-hello-node) <none>
2024-12-20 09:01:58 +0000

    🍍🗻 Checkpoint ./server.js:72855240/552

--- server.js
+++ server.js
@@ -11,18 +11,20 @@ mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
 // Define a schema
 const userSchema = new mongoose.Schema({
   name: String,
-  age: Number,
-  email: String,
+  totalBeds: Number,
+  occupiedBeds: Number,
+  reserveBeds: Number,
+  availableBeds: Number,
 });
 
 // Create a model
-const Data = mongoose.model('data', userSchema);
+const icuList = mongoose.model('icuList', userSchema);
 
 // Fetch and print data
 async function fetchData() {
   try {
-    const users = await Data.find(); // Fetch all documents from the "users" collection
-    console.log('Data from the database:', users);
+    const icudata = await icuList.find(); // Fetch all documents from the "users" collection
+    console.log('Data from the database:', icudata);
   } catch (err) {
     console.error('Error fetching data:', err);
   } finally {
