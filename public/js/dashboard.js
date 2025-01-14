6046c8110122323fa26fb0de7e9c65228d18e064
Glitch (glitch-hello-node) <none>
2025-01-14 06:40:34 +0000

    🎸😄 Checkpoint ./public/css/_common.css:71079836/1069 ./public/css/_dashboard.css:71079836/2656 ./public/js/main.js:71079836/730 ./.glitch-assets:71079836/788 ./public/index.html:71079836/360 ./public/js/dashboard.js:71079836/6619

---
+++ public/js/dashboard.js
@@ -0,0 +1,15 @@
+$( document ).ready(function() {
+   async function getData() {
+        try {
+          const response = await fetch("/components");
+          const components = await response.json();
+          
+          console.log("aaaa"+components)
+
+      
+        } catch (error) {
+          console.error("Error fetching components:", error);
+        }
+   }
+  getData();
+});
