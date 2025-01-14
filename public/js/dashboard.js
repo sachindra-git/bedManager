44ec9044a2565a4e0fcb40162b839a2256bcfa13
Glitch (glitch-hello-node) <none>
2025-01-14 14:03:43 +0000

    🚔👌 Checkpoint ./public/js/dashboard.js:71079836/4149

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -11,6 +11,9 @@ $( document ).ready(function() {
           let totalAvailableBeds = 0;
           let totalReserveBeds = 0;
           let totalOccupiedBeds = 0;
+          let totalAvailableBedsPerc = 0;
+          let totalReserveBedsPerc = 0;
+          let totalOccupiedBedsPerc = 0;
           let totalAvailableBedsCount = '';
           let wrapperDiv;
           
@@ -22,6 +25,9 @@ $( document ).ready(function() {
             totalAvailableBeds += data.availableBeds;
             totalReserveBeds += data.reserveBeds;
             totalOccupiedBeds += data.occupiedBeds;
+            totalAvailableBedsPerc = totalAvailableBeds/totalBeds * 100;
+            totalReserveBedsPerc = totalAvailableBeds/totalBeds * 100;
+            totalOccupiedBedsPerc = totalAvailableBeds/totalBeds * 100;
           
             if( data.availableBeds > 0 && index < 9 ) {
               console.log(index,'indexindexindexindexindexindex');
@@ -45,7 +51,7 @@ $( document ).ready(function() {
             }
             
           });
-          
+          console.log(totalAvailableBedsPerc, 'totalAvailableBedsPerctotalAvailableBedsPerctotalAvailableBedsPerc');
           
             if(totalAvailableBeds < 1) {
               totalAvailableBedsCount = 'No';
@@ -62,7 +68,7 @@ $( document ).ready(function() {
           let myChart = new Chart(chartEl, {
               type: 'pie',
               data: {
-                labels: ["Total Occupied Beds",	"Total Reserve Beds",	"Total Available Beds"],
+                labels: ["Total Occupied Beds [" +parseFloat(totalAvailableBedsPerc.toFixed(2))+"%]",	"Total Reserve Beds",	"Total Available Beds"],
                 datasets: [{    
                     data: [totalOccupiedBeds,	totalReserveBeds,	totalAvailableBeds],
 
@@ -73,6 +79,16 @@ $( document ).ready(function() {
               options: {
                 responsive: true,
                 maintainAspectRatio: false, 
+								legend: {
+													display: true,
+													position: "right",
+													labels: {
+																			boxWidth: 30,
+																			fontColor: "#999",
+																			fontFamily: "Montserrat",
+																			fullWidth: true
+													} 
+									},
               }
           });
       
