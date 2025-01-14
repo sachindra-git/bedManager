b8118f5dd8f1236a481eca90a529fc855dd7a62a
Glitch (glitch-hello-node) <none>
2025-01-14 07:50:40 +0000

    🏮👨‍👨‍👦 Checkpoint ./public/js/dashboard.js:71079836/746

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -5,21 +5,27 @@ $( document ).ready(function() {
           const components = await response.json();
           const totalICUEl = document.querySelector('.total-icus .count');
           const totalBedEl = document.querySelector('.total-beds .count');
+          const availableBedsEl = document.querySelector('.available_count .count');
           let totalBeds = 0;
+          let totalAvailableBeds = 0;
           
           console.log(components.length, 'ssssssssssss')
           totalICUEl.innerHTML= components.length;
           
           components.forEach(data => {
             totalBeds += data.totalBeds;
+            //totalAvailableBeds += data.availableBeds;
             
-            if(data.availableBeds > 0) {
-              console.log(data.name, 'nameeeeeeeeeee');
+            if(totalAvailableBeds < 1) {
+              totalAvailableBeds = 'No';
+            } else {
+              totalAvailableBeds += data.availableBeds;
             }
             
           });
           
           totalBedEl.innerHTML= totalBeds;
+          availableBedsEl.innerHTML= totalAvailableBeds;
       
         } catch (error) {
           console.error("Error fetching components:", error);
