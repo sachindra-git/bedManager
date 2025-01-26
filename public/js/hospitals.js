1cd204b76878c1cb04279f589d30cd67e39b8efb
Glitch (glitch-hello-node) <none>
2025-01-26 07:20:40 +0000

    🌂🏌 Checkpoint ./public/js/hospitals.js:71079836/9356

--- public/js/hospitals.js
+++ public/js/hospitals.js
@@ -7,63 +7,70 @@ $( document ).ready(function() {
           const hospitalTableWrap = document.querySelector('.hospital-table .table_body');
           const totalHospitalsEl = document.querySelector('.total-content .total-hospitals');
           let totalHospitals = hospitals.length;
+          let filteredHospitals = hospitals;
           let totalICU = 0;
           let HoswrapperDiv;
           
           const searchBar = document.querySelector("#hospital-search");
+                  
           
-  searchBar.addEventListener("input", (e) => {
-    const searchTerm = e.target.value.toLowerCase(); // Convert to lowercase for case-insensitive matching
+          
+          function displayHospitals(filteredHospitals) {
+            hospitalTableWrap.innerHTML = "";
+            filteredHospitals.forEach((data, index) => {
+                HoswrapperDiv = document.createElement('DIV');
+                const newDiv2 = document.createElement('DIV');
+                const newDiv3 = document.createElement('DIV');
+                const newDiv4 = document.createElement('DIV');
+                const newDiv5 = document.createElement('DIV');
+                const newDiv6 = document.createElement('DIV');
+                const anchor = document.createElement('a');
 
-    // Filter the hospitals based on the search term
-    const hospitals = hospitals.filter((hospital) =>
-      hospital.name.toLowerCase().includes(searchTerm)
-    );
-    
-    console.log(hospitals, 'filterrrrrrrrrrrr')
 
-    // Process the filtered data
-    // filteredHospitals.forEach((data, index) => {
-    //   console.log(`Hospital ${index + 1}:`, data.name); // Example output
-    // });
-  });
+                HoswrapperDiv.classList.add('table_row');
+                newDiv2.classList.add('name');
+                newDiv3.classList.add('district');
+                newDiv4.classList.add('province');
+                newDiv5.classList.add('count');
+                newDiv6.classList.add('contact');
+                anchor.classList.add('contact-link');
+                anchor.href = 'tel:'+data.contact;
+                newDiv6.appendChild(anchor);
+                newDiv2.innerHTML += data.name;
+                newDiv3.innerHTML += data.district;
+                newDiv4.innerHTML += data.province;
+                newDiv5.innerHTML += data.totalIcus;
+                anchor.innerHTML += data.contact;
+
+
+
+                HoswrapperDiv.appendChild(newDiv2);
+                HoswrapperDiv.appendChild(newDiv3);
+                HoswrapperDiv.appendChild(newDiv4);
+                HoswrapperDiv.appendChild(newDiv5);
+                HoswrapperDiv.appendChild(newDiv6);
+
+                hospitalTableWrap.appendChild(HoswrapperDiv);    
+            });
+          }
         
  
-          hospitals.forEach((data, index) => {
-              HoswrapperDiv = document.createElement('DIV');
-              const newDiv2 = document.createElement('DIV');
-              const newDiv3 = document.createElement('DIV');
-              const newDiv4 = document.createElement('DIV');
-              const newDiv5 = document.createElement('DIV');
-              const newDiv6 = document.createElement('DIV');
-              const anchor = document.createElement('a');
-            
-            
-              HoswrapperDiv.classList.add('table_row');
-              newDiv2.classList.add('name');
-              newDiv3.classList.add('district');
-              newDiv4.classList.add('province');
-              newDiv5.classList.add('count');
-              newDiv6.classList.add('contact');
-              anchor.classList.add('contact-link');
-              anchor.href = 'tel:'+data.contact;
-              newDiv6.appendChild(anchor);
-              newDiv2.innerHTML += data.name;
-              newDiv3.innerHTML += data.district;
-              newDiv4.innerHTML += data.province;
-              newDiv5.innerHTML += data.totalIcus;
-              anchor.innerHTML += data.contact;
-            
-              
-              
-              HoswrapperDiv.appendChild(newDiv2);
-              HoswrapperDiv.appendChild(newDiv3);
-              HoswrapperDiv.appendChild(newDiv4);
-              HoswrapperDiv.appendChild(newDiv5);
-              HoswrapperDiv.appendChild(newDiv6);
-              
-              hospitalTableWrap.appendChild(HoswrapperDiv);    
-          });
+          
+          searchBar.addEventListener("input", (e) => {
+            const searchTerm = e.target.value.toLowerCase(); // Convert to lowercase for case-insensitive matching
+
+            // Filter the hospitals based on the search term
+            const filteredHospitals = hospitals.filter((hospital) =>
+              hospital.name.toLowerCase().includes(searchTerm)
+            );
+
+            console.log(filteredHospitals, 'filterrrrrrrrrrrr')
+
+            displayHospitals(filteredHospitals);
+
+          });    
+          
+          displayHospitals(hospitals);
           
           totalHospitalsEl.innerHTML = totalHospitals;
           
