a1088d969fc3a9e04854c9c8f4a410e2ce6bf6f1
Glitch (glitch-hello-node) <none>
2025-01-26 06:00:55 +0000

    🚐⛩ Checkpoint ./public/js/hospitals.js:71079836/2968

--- public/js/hospitals.js
+++ public/js/hospitals.js
@@ -4,29 +4,41 @@ $( document ).ready(function() {
         try {
           const response = await fetch("/hospitals");
           const hospitals = await response.json();
+          const hospitalTableWrap = document.querySelector('.hospital-table .table_body');
+          let HoswrapperDiv;
  
           hospitals.forEach((data, index) => {
-            if( index < 9 ) {
+            console.log(data, 'aaaaaaaaaaaaa');
               HoswrapperDiv = document.createElement('DIV');
               const newDiv2 = document.createElement('DIV');
               const newDiv3 = document.createElement('DIV');
+              const newDiv4 = document.createElement('DIV');
+              const newDiv5 = document.createElement('DIV');
+              const newDiv6 = document.createElement('DIV');
+              const anchor = document.createElement('A');
               HoswrapperDiv.classList.add('table_row');
               newDiv2.classList.add('name');
-              newDiv3.classList.add('hospital-count');
+              newDiv3.classList.add('district');
+              newDiv4.classList.add('province');
+              newDiv5.classList.add('count');
+              newDiv6.classList.add('contact');
+              anchor.classList.add('contact-link');
               
               newDiv2.innerHTML += data.name;
-              newDiv3.innerHTML += data.totalIcus;
+              newDiv3.innerHTML += data.district;
+              newDiv4.innerHTML += data.province;
+              newDiv5.innerHTML += data.totalIcus;
+              newDiv6.innerHTML += data.contact;
               
               HoswrapperDiv.appendChild(newDiv2);
               HoswrapperDiv.appendChild(newDiv3);
+              HoswrapperDiv.appendChild(newDiv4);
+              HoswrapperDiv.appendChild(newDiv5);
+              HoswrapperDiv.appendChild(newDiv6);
               
-              hospitalTableWrap.appendChild(HoswrapperDiv);
-              
-            }
-            
+              hospitalTableWrap.appendChild(HoswrapperDiv);    
           });
           
-          totalHospitalsEl.innerHTML = totalHospitals;
             
         } catch (error) {
           console.error("Error fetching hospitals:", error);
