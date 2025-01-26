39eafb3d8c637ac3a16719adb05b6822f35a5e49
Glitch (glitch-hello-node) <none>
2025-01-26 06:12:04 +0000

    🍮🦄 Checkpoint ./public/js/hospitals.js:71079836/230 ./public/css/_common.css:71079836/208 ./public/css/_hospital.css:71079836/520

--- public/js/hospitals.js
+++ public/js/hospitals.js
@@ -15,7 +15,7 @@ $( document ).ready(function() {
               const newDiv4 = document.createElement('DIV');
               const newDiv5 = document.createElement('DIV');
               const newDiv6 = document.createElement('DIV');
-              const anchor = document.createElement('A');
+              const anchor = document.createElement('a');
               HoswrapperDiv.classList.add('table_row');
               newDiv2.classList.add('name');
               newDiv3.classList.add('district');
@@ -23,12 +23,15 @@ $( document ).ready(function() {
               newDiv5.classList.add('count');
               newDiv6.classList.add('contact');
               anchor.classList.add('contact-link');
-              
+              anchor.href = 'tel:'+data.contact;
+              newDiv6.appendChild(anchor);
               newDiv2.innerHTML += data.name;
               newDiv3.innerHTML += data.district;
               newDiv4.innerHTML += data.province;
               newDiv5.innerHTML += data.totalIcus;
-              newDiv6.innerHTML += data.contact;
+              anchor.innerHTML += data.contact;
+            
+              
               
               HoswrapperDiv.appendChild(newDiv2);
               HoswrapperDiv.appendChild(newDiv3);
