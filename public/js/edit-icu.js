46240cb9a96af28cfd8f1c4951a8336ca3cf91a3
Glitch (glitch-hello-node) <none>
2025-02-03 08:22:21 +0000

    🦄🍼 Checkpoint ./public/js/edit-icu.js:71079836/885

--- public/js/edit-icu.js
+++ public/js/edit-icu.js
@@ -6,6 +6,26 @@ async function getICUdata() {
     const icus = await response.json();
     
 
+    function displayFormData() {
+      
+    }
+    
+    
+    function displayIcuDropdown(icuData) {
+      const select = document.querySelector('#icuName');
+      const option = document.createElement('OPTION');
+      option.innerHTML = icuData.name;
+      option.value = icuData.id;
+      
+      select.appendChild(option);
+      
+    }
+    
+    icus.forEach((icuData) => {
+      displayIcuDropdown(icuData);
+    });
+    
+    
 
   } catch (error) {
     console.error("Error fetching icus:", error);
