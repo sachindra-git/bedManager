6754f08da8bf53c4be4cd00f46faa13704339637
Glitch (glitch-hello-node) <none>
2025-02-04 03:48:35 +0000

    🍓⚓️ Checkpoint ./public/js/bed-request.js:71079836/2074 ./public/css/_bed-request.css:71079836/1468

--- public/js/bed-request.js
+++ public/js/bed-request.js
@@ -21,6 +21,7 @@ async function getReqdata() {
       }
 
       reqPage.forEach((data) => {
+        console.log(data, 'datadatadatadatadatadata')
         const reqWrapperDiv = document.createElement('DIV');
         const newDiv2 = document.createElement('DIV');
         const newDiv3 = document.createElement('DIV');
@@ -28,27 +29,26 @@ async function getReqdata() {
         const newDiv5 = document.createElement('DIV');
         const newDiv6 = document.createElement('DIV');
         const newDiv7 = document.createElement('DIV');
-        const anchor = document.createElement('a');
-        const editAnchor = document.createElement('a');
+        const newDiv8 = document.createElement('DIV');
+
 
         reqWrapperDiv.classList.add('table_row');
         newDiv2.classList.add('date');
-        newDiv3.classList.add('p-age');
-        newDiv4.classList.add('hos-name');
-        newDiv5.classList.add('ward-no');
-        newDiv6.classList.add('p-status');
-        newDiv7.classList.add('req-status');
+        newDiv3.classList.add('p-name');
+        newDiv4.classList.add('p-age');
+        newDiv5.classList.add('hos-name');
+        newDiv6.classList.add('ward-no');
+        newDiv7.classList.add('p-status');
+        newDiv8.classList.add('req-status');
 
         // Fill content
-        newDiv2.innerHTML = data.name;
-        newDiv3.innerHTML = data.totalBeds;
-        newDiv4.innerHTML = data.occupiedBeds;
-        newDiv5.innerHTML = data.reserveBeds;
-        newDiv6.innerHTML = data.availableBeds;
-        anchor.innerHTML = data.contact;
-        newDiv7.appendChild(anchor);
-        editAnchor.innerHTML = '<svg width="13px" height="13px" viewBox="-2.56 0 89.725 89.725" xmlns="http://www.w3.org/2000/svg"><g id="Group_11" data-name="Group 11" transform="translate(-1020.3 -668.175)"><path id="Path_53" data-name="Path 53" d="M1066.1,682.8l-34.8,34.8a3.858,3.858,0,0,0-1.1,2.2l-.8,10.1a2.488,2.488,0,0,0,2.8,2.8l9.8-.8a3.857,3.857,0,0,0,2.2-1.1l35-35a3.041,3.041,0,0,0,.3-4.3l-9.1-9.1A3.052,3.052,0,0,0,1066.1,682.8Z" fill="none" stroke="#2b4ea2" stroke-miterlimit="10" stroke-width="4"/><path id="Path_54" data-name="Path 54" d="M1079.6,690.2l-7.8-7.8a3.684,3.684,0,0,1,0-5.3l5.8-5.8a3.684,3.684,0,0,1,5.3,0l7.8,7.8a3.684,3.684,0,0,1,0,5.3l-5.8,5.8A3.869,3.869,0,0,1,1079.6,690.2Z" fill="none" stroke="#2b4ea2" stroke-linecap="round" stroke-miterlimit="10" stroke-width="4"/><path id="Path_55" data-name="Path 55" d="M1098.6,755.9h-72a4.268,4.268,0,0,1-4.3-4.3v-3.3a4.268,4.268,0,0,1,4.3-4.3h72a4.268,4.268,0,0,1,4.3,4.3v3.3A4.268,4.268,0,0,1,1098.6,755.9Z" fill="none" stroke="#2b4ea2" stroke-linecap="round" stroke-miterlimit="10" stroke-width="4"/><path id="Path_56" data-name="Path 56" d="M1103.5,739.8" fill="none" stroke="#2b4ea2" stroke-linecap="round" stroke-miterlimit="10" stroke-width="4"/></g></svg>';
-        newDiv2.appendChild(editAnchor);
+        newDiv2.innerHTML = data.reqDate;
+        newDiv3.innerHTML = data.patientName;
+        newDiv4.innerHTML = data.patientAge;
+        newDiv5.innerHTML = data.hospitalName;
+        newDiv6.innerHTML = data.wardNumber;
+        newDiv7.innerHTML = data.patientStatus;
+        newDiv8.innerHTML = data.bedRequestStatus;
 
         // Build row
         reqWrapperDiv.appendChild(newDiv2);
@@ -57,6 +57,7 @@ async function getReqdata() {
         reqWrapperDiv.appendChild(newDiv5);
         reqWrapperDiv.appendChild(newDiv6);
         reqWrapperDiv.appendChild(newDiv7);
+        reqWrapperDiv.appendChild(newDiv8);
 
         // Append to table
         reqTableWrap.appendChild(reqWrapperDiv);
