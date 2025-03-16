304db7072586f765bcc61a65db891ed3453b393a
Glitch (glitch-hello-node) <none>
2025-03-16 07:27:06 +0000

    👸🌑 Checkpoint ./public/js/add-icu.js:71079836/1338

--- public/js/add-icu.js
+++ public/js/add-icu.js
@@ -1,15 +1,14 @@
 $( document ).ready(function() {
   
-  async function getICUdata(newICUname) {
+  const icuNameList = [];
+  
+  async function getICUNames() {
     try {
       const response = await fetch("/icus");
       const icus = await response.json();
-      $('#icuName').select2();
 
       icus.forEach((icuData) => {
-        if(icuData.name == newICUname) {
-          conso
-        }
+        icuNameList.push(icuData.name);
       });
      
 
@@ -18,7 +17,7 @@ $( document ).ready(function() {
     }
   }
   
-  
+  getICUNames();
   
   
   
@@ -36,37 +35,40 @@ $( document ).ready(function() {
         contact: document.getElementById('contactNumber').value,
       };
       
+      
       if( parseInt(formDataObject.occupiedBeds) + parseInt(formDataObject.availableBeds) + parseInt(formDataObject.reserveBeds) != parseInt(formDataObject.totalBeds) ) {
         document.getElementById('error-message-wrap').innerHTML = `<div class="error-message">Please check the entered bed Counts. Total Bed Count did not match</div>`;
+      } else if (icuNameList.includes(formDataObject.name)) {
+        document.getElementById('error-message-wrap').innerHTML = `<div class="error-message">The ICU already exists.</div>`;
       } else {
         
-        fetch('/add', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(formDataObject)
-        })
-        .then(response => response.text())
-        .then(data => {
-          document.getElementById('error-message-wrap').innerHTML = '';
-          document.body.classList.add('data-saving');
-          setTimeout(() => {
-            document.getElementById('icuForm').reset();
-            document.getElementById('message').style.display = 'block';
-            document.getElementById('message').innerHTML = `<div class="success-message">ICU added Successfully</div>`;
-            document.body.classList.remove('data-saving');
-          }, 3000);
+//         fetch('/add', {
+//           method: 'POST',
+//           headers: {
+//             'Content-Type': 'application/json'
+//           },
+//           body: JSON.stringify(formDataObject)
+//         })
+//         .then(response => response.text())
+//         .then(data => {
+//           document.getElementById('error-message-wrap').innerHTML = '';
+//           document.body.classList.add('data-saving');
+//           setTimeout(() => {
+//             document.getElementById('icuForm').reset();
+//             document.getElementById('message').style.display = 'block';
+//             document.getElementById('message').innerHTML = `<div class="success-message">ICU added Successfully</div>`;
+//             document.body.classList.remove('data-saving');
+//           }, 3000);
 
-          setTimeout(() => {
-            document.getElementById('message').style.display = 'none';
-          }, 6000);
-        })
-        .catch(error => {
-          // Handle errors
-          console.error('Error:', error);
-          document.getElementById('error-message-wrap').innerHTML = `<div class="error-message">Error updating ICU. Please try again later.</div>`;
-        }); 
+//           setTimeout(() => {
+//             document.getElementById('message').style.display = 'none';
+//           }, 6000);
+//         })
+//         .catch(error => {
+//           // Handle errors
+//           console.error('Error:', error);
+//           document.getElementById('error-message-wrap').innerHTML = `<div class="error-message">Error updating ICU. Please try again later.</div>`;
+//         }); 
       }
     });
   }
