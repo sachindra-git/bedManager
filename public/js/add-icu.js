12f884e44aff445a2d8be66786d6a0c21394e3e4
Glitch (glitch-hello-node) <none>
2025-03-16 07:03:06 +0000

    💑🍥 Checkpoint ./public/js/add-icu.js:71079836/2568

--- public/js/add-icu.js
+++ public/js/add-icu.js
@@ -1,5 +1,68 @@
 $( document ).ready(function() {
   
+  async function getICUdata() {
+    try {
+      const response = await fetch("/icus");
+      const icus = await response.json();
+      $('#icuName').select2();
+
+
+      function displayICUdataOnForm(selectedValue) {
+        icus.forEach((icuData) => {
+          let selectedValue = document.getElementById("icuName").value;
+          if( icuData._id == selectedValue ) {
+            const totalInput = document.querySelector('#totalBeds');
+            const contact = document.querySelector('#contactNumber');
+            const occupiedInput = document.querySelector('#occupiedBeds');
+            const reserveInput = document.querySelector('#reserveBeds');
+            const availableInput = document.querySelector('#availableBeds');
+            const totalBeds = icuData.totalBeds;
+            const occupiedBeds = icuData.occupiedBeds;
+            const reserveBeds = icuData.reserveBeds;
+            const availableBeds = icuData.availableBeds;
+            const contactNumber = icuData.contact;
+
+            totalInput.value = totalBeds
+            occupiedInput.value = occupiedBeds
+            reserveInput.value = reserveBeds
+            availableInput.value = availableBeds
+            contact.value = contactNumber;
+          }
+        });
+      }
+
+      $('#icuName').on('select2:select', function (e) {
+        let selectedValue = $(this).val();
+        displayICUdataOnForm(selectedValue);   
+      });
+
+
+      function displayIcuDropdown(icuData) {
+        const select = document.querySelector('#icuName');
+        const option = document.createElement('OPTION');
+        option.innerHTML = icuData.name;
+        option.value = icuData._id;
+
+        select.appendChild(option);
+
+      }
+
+      icus.forEach((icuData) => {
+        displayIcuDropdown(icuData);
+      });
+
+
+
+    } catch (error) {
+      console.error("Error fetching icus:", error);
+    }
+  }
+  
+  
+  
+  
+  
+  
   function formSubmit() {
     document.getElementById('icuForm').addEventListener('submit', function(event) {
       event.preventDefault();
