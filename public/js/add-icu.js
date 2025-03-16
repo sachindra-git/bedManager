5a52c89a8a045e30852aecaba499e7d3771d4e7b
Glitch (glitch-hello-node) <none>
2025-03-16 07:39:06 +0000

    👐🦂 Checkpoint ./public/js/add-icu.js:71079836/78 ./public/hospitals.html:71079836/281

--- public/js/add-icu.js
+++ public/js/add-icu.js
@@ -42,33 +42,33 @@ $( document ).ready(function() {
         document.getElementById('error-message-wrap').innerHTML = `<div class="error-message">The ICU already exists.</div>`;
       } else {
         
-//         fetch('/add', {
-//           method: 'POST',
-//           headers: {
-//             'Content-Type': 'application/json'
-//           },
-//           body: JSON.stringify(formDataObject)
-//         })
-//         .then(response => response.text())
-//         .then(data => {
-//           document.getElementById('error-message-wrap').innerHTML = '';
-//           document.body.classList.add('data-saving');
-//           setTimeout(() => {
-//             document.getElementById('icuForm').reset();
-//             document.getElementById('message').style.display = 'block';
-//             document.getElementById('message').innerHTML = `<div class="success-message">ICU added Successfully</div>`;
-//             document.body.classList.remove('data-saving');
-//           }, 3000);
+        fetch('/add', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(formDataObject)
+        })
+        .then(response => response.text())
+        .then(data => {
+          document.getElementById('error-message-wrap').innerHTML = '';
+          document.body.classList.add('data-saving');
+          setTimeout(() => {
+            document.getElementById('icuForm').reset();
+            document.getElementById('message').style.display = 'block';
+            document.getElementById('message').innerHTML = `<div class="success-message">ICU added Successfully</div>`;
+            document.body.classList.remove('data-saving');
+          }, 3000);
 
-//           setTimeout(() => {
-//             document.getElementById('message').style.display = 'none';
-//           }, 6000);
-//         })
-//         .catch(error => {
-//           // Handle errors
-//           console.error('Error:', error);
-//           document.getElementById('error-message-wrap').innerHTML = `<div class="error-message">Error updating ICU. Please try again later.</div>`;
-//         }); 
+          setTimeout(() => {
+            document.getElementById('message').style.display = 'none';
+          }, 6000);
+        })
+        .catch(error => {
+          // Handle errors
+          console.error('Error:', error);
+          document.getElementById('error-message-wrap').innerHTML = `<div class="error-message">Error updating ICU. Please try again later.</div>`;
+        }); 
       }
     });
   }
