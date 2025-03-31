84812237f1ab4afdeb640a29454ae3556f2cf54d
Glitch (glitch-hello-node) <none>
2025-03-31 12:57:25 +0000

    💿🍗 Checkpoint ./public/js/login.js:71079836/2589 ./public/login.html:71079836/224 ./public/css/_login.css:71079836/51

--- public/js/login.js
+++ public/js/login.js
@@ -2,8 +2,15 @@ $( document ).ready(function() {
   
   async function formSubmit() {
     
-  const response = await fetch("/user");
-  const users = await response.json();
+    const response = await fetch("/user");
+    const users = await response.json();
+    let userNames = [];
+    let passwords = [];
+
+    users.forEach((user) => {
+      userNames.push(user.userName);
+      passwords.push(user.password);
+    });
     
     document.getElementById('loginForm').addEventListener('submit', function(event) {
       event.preventDefault();
@@ -12,16 +19,18 @@ $( document ).ready(function() {
         userName: document.getElementById('userName').value.trim(),
         password: document.getElementById('password').value.trim(),
       };
-      
-      
+
+
       try {
 
-      
-        users.forEach((user) => {
-          
-        });
+        if(formDataObject.userName == '') {
+          document.getElementById('error-message-wrap').innerHTML = `<div class="error-message">Please Enter Username</div>`
+        }
 
-        
+
+
+
+
 
         console.log("Form submitted successfully:", formDataObject);
         // You can proceed with further actions like sending data to a server
@@ -30,41 +39,9 @@ $( document ).ready(function() {
         console.error("Error:", error.message);
         alert(error.message); // Display error message to the user
       }
-      
-      
-//       fetch('/addBedReq', {
-//         method: 'POST',
-//         headers: {
-//           'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify(formDataObject)
-//       })
-//       .then(response => response.text())
-//       .then(data => {
-//         const form = document.querySelector('#bedReqForm');
-//         document.getElementById('error-message-wrap').innerHTML = '';
-//         document.body.classList.add('data-saving');
-//         form.querySelector('.submit-wrapper button').classList.add('button-disabled');
-//         setTimeout(() => {
-//           document.getElementById('bedReqForm').reset();
-//           document.getElementById('message').style.display = 'block';
-//           document.getElementById('message').innerHTML = `<div class="success-message">Bed Request added Successfully</div>`;
-//           document.body.classList.remove('data-saving');
-//           form.querySelector('.submit-wrapper button').classList.remove('button-disabled');
-//         }, 3000);
 
-//         setTimeout(() => {
-//           document.getElementById('message').style.display = 'none';
-//         }, 6000);
-//       })
-//       .catch(error => {
-//         // Handle errors
-//         console.error('Error:', error);
-//         document.getElementById('error-message-wrap').innerHTML = `<div class="error-message">Error adding Bed Request. Please try again later.</div>`;
-//       }); 
-      
-    });
-  }
+  });
+ }
 
   formSubmit();
   
