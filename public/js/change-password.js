7fe01e195bd7e9eb3a0bc447904b8f92cf1f05e7
Glitch (glitch-hello-node) <none>
2025-03-31 16:34:27 +0000

    🐸⏱ Checkpoint ./public/js/change-password.js:71079836/6381 ./public/js/main.js:71079836/310

--- public/js/change-password.js
+++ public/js/change-password.js
@@ -37,86 +37,73 @@ $( document ).ready(function() {
         password: document.getElementById('newPassword').value.trim(),
       };
       
-        if( formDataObject.currentPassword == '' ) {
-          document.getElementById('error-message-wrap').innerHTML = `<div class="error-message">Please enter Current Password</div>`;
-        } else if( formDataObject.newPassword == '' ) {
-          document.getElementById('error-message-wrap').innerHTML = `<div class="error-message">Please enter new Password</div>`;
-        } else if( formDataObject.RenewPassword == '' ) {
-          document.getElementById('error-message-wrap').innerHTML = `<div class="error-message">Please retype new Password</div>`;
-        } else if( formDataObject.currentPassword != currentPassword ) {
-          document.getElementById('error-message-wrap').innerHTML = `<div class="error-message">Please enter correct Password</div>`;
-        } else if( formDataObject.newPassword != '' ) {
-          const minLength = 8;
-          const hasUpperCase = /[A-Z]/.test(formDataObject.newPassword);
-          const hasLowerCase = /[a-z]/.test(formDataObject.newPassword);
-          const hasNumber = /[0-9]/.test(formDataObject.newPassword);
-          const hasSpecialChar = /[@$!%*?&#]/.test(formDataObject.newPassword);
-
-          if (formDataObject.newPassword.length < minLength) {
-            document.getElementById('error-message-wrap').innerHTML = `<div class="error-message">Password must be at least 8 characters long.</div>`;
-          } else if (!hasUpperCase) {
-            document.getElementById('error-message-wrap').innerHTML = `<div class="error-message">Password must contain at least one uppercase letter.</div>`;
-          } else if (!hasLowerCase) {
-            document.getElementById('error-message-wrap').innerHTML = `<div class="error-message">Password must contain at least one lowercase letter.</div>`;
-          } else if (!hasNumber) {
-            document.getElementById('error-message-wrap').innerHTML = `<div class="error-message">Password must contain at least one number.</div>`;
-          } else if (!hasSpecialChar) {
-            document.getElementById('error-message-wrap').innerHTML = `<div class="error-message">Password must contain at least one special character (@, $, !, %, etc.).</div>`;
-          } else {
-            document.getElementById('error-message-wrap').innerHTML = "";
-          }
-        } else if( formDataObject.newPassword != formDataObject.RenewPassword ) {
-          document.getElementById('error-message-wrap').innerHTML = `<div class="error-message">New Pasword did not match</div>`;
-        } else {
-          
-          alert('success');
-          
-          fetch('/changePassword', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(sendingFormDataObject)
-          })
-          .then(response => response.text())
-          .then(data => {
-            const form = document.querySelector('#changePassword');
-
-
-
-
-            document.getElementById('error-message-wrap').innerHTML = '';
-            document.body.classList.add('data-saving');
-            form.querySelector('.submit-wrapper button').classList.add('button-disabled');
-            setTimeout(() => {
-              document.getElementById('changePassword').reset();
-              document.getElementById('message').style.display = 'block';
-              document.getElementById('message').innerHTML = `<div class="success-message">Bed Request added Successfully</div>`;
-              document.body.classList.remove('data-saving');
-              form.querySelector('.submit-wrapper button').classList.remove('button-disabled');
-            }, 3000);
-
-            setTimeout(() => {
-              document.getElementById('message').style.display = 'none';
-            }, 6000);
-
-
-
-          })
-          .catch(error => {
-            // Handle errors
-            console.error('Error:', error);
-            document.getElementById('error-message-wrap').innerHTML = `<div class="error-message">Error Change Password. Please try again later.</div>`;
-          }); 
-          
-          
-          
-          
-          
-        }
+      const minLength = 8;
+      const hasUpperCase = /[A-Z]/.test(formDataObject.newPassword);
+      const hasLowerCase = /[a-z]/.test(formDataObject.newPassword);
+      const hasNumber = /[0-9]/.test(formDataObject.newPassword);
+      const hasSpecialChar = /[@$!%*?&#]/.test(formDataObject.newPassword);
       
+      if( formDataObject.currentPassword == '' ) {
+        document.getElementById('error-message-wrap').innerHTML = `<div class="error-message">Please enter Current Password</div>`;
+      } else if( formDataObject.newPassword == '' ) {
+        document.getElementById('error-message-wrap').innerHTML = `<div class="error-message">Please enter new Password</div>`;
+      } else if( formDataObject.RenewPassword == '' ) {
+        document.getElementById('error-message-wrap').innerHTML = `<div class="error-message">Please retype new Password</div>`;
+      } else if( formDataObject.currentPassword != currentPassword ) {
+        document.getElementById('error-message-wrap').innerHTML = `<div class="error-message">Please enter correct Password</div>`;
+      } else if (formDataObject.newPassword.length < minLength) {
+        document.getElementById('error-message-wrap').innerHTML = `<div class="error-message">Password must be at least 8 characters long.</div>`;
+      } else if (!hasUpperCase) {
+        document.getElementById('error-message-wrap').innerHTML = `<div class="error-message">Password must contain at least one uppercase letter.</div>`;
+      } else if (!hasLowerCase) {
+        document.getElementById('error-message-wrap').innerHTML = `<div class="error-message">Password must contain at least one lowercase letter.</div>`;
+      } else if (!hasNumber) {
+        document.getElementById('error-message-wrap').innerHTML = `<div class="error-message">Password must contain at least one number.</div>`;
+      } else if (!hasSpecialChar) {
+        document.getElementById('error-message-wrap').innerHTML = `<div class="error-message">Password must contain at least one special character (@, $, !, %, etc.).</div>`;
+      } else if( formDataObject.newPassword != formDataObject.RenewPassword ) {
+        document.getElementById('error-message-wrap').innerHTML = `<div class="error-message">New Pasword did not match</div>`;
+      } else {
       
-      
+        document.getElementById('error-message-wrap').innerHTML = "";
+
+        fetch('/changePassword', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(sendingFormDataObject)
+        })
+        .then(response => response.text())
+        .then(data => {
+          const form = document.querySelector('#changePassword');
+          
+          document.getElementById('error-message-wrap').innerHTML = '';
+          document.body.classList.add('data-saving');
+          form.querySelector('.submit-wrapper button').classList.add('button-disabled');
+          setTimeout(() => {
+            document.getElementById('changePassword').reset();
+            document.getElementById('message').style.display = 'block';
+            document.getElementById('message').innerHTML = `<div class="success-message">Password updated Successfully</div>`;
+            document.body.classList.remove('data-saving');
+            form.querySelector('.submit-wrapper button').classList.remove('button-disabled');
+          }, 3000);
+
+          setTimeout(() => {
+            document.getElementById('message').style.display = 'none';
+            if(sessionStorage.getItem("loggedInUser")) {
+              sessionStorage.removeItem("loggedInUser");
+              window.location.href = "login.html";
+            }
+          }, 6000);
+
+        })
+        .catch(error => {
+          // Handle errors
+          console.error('Error:', error);
+          document.getElementById('error-message-wrap').innerHTML = `<div class="error-message">Error Change Password. Please try again later.</div>`;
+        }); 
+      }
       
     });
   }
