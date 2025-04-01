61aa0d1e99a717b98a38d91bd193949019d87fbe
Glitch (glitch-hello-node) <none>
2025-04-01 15:37:16 +0000

    🌗🎿 Checkpoint ./public/js/add-user.js:71079836/2291 ./public/add-user.html:71079836/4

--- public/js/add-user.js
+++ public/js/add-user.js
@@ -2,37 +2,59 @@ $( document ).ready(function() {
 
   $('#userType').select2();
   
+  let selectedValue;
+  
   function formSubmit() {
     document.getElementById('addUser').addEventListener('submit', function(event) {
       event.preventDefault();
       
     $('#userType').on('select2:select', function (e) {
-      let selectedValue = $(this).val();
-      displayICUdataOnForm(selectedValue);   
+      selectedValue = $(this).val();
     });
 
       const formDataObject = {
         userName: document.getElementById('userName').value.trim(),
         password: document.getElementById('password').value.trim(),
         RenewPassword: document.getElementById('RenewPassword').value.trim(),
+        userType: selectedValue
       };
       
       const sendingFormDataObject = {
         userName: document.getElementById('userName').value.trim(),
         password: document.getElementById('newPassword').value.trim(),
+        userType: selectedValue
       };
       
+      const minLength = 8;
+      const hasUpperCase = /[A-Z]/.test(formDataObject.password);
+      const hasLowerCase = /[a-z]/.test(formDataObject.password);
+      const hasNumber = /[0-9]/.test(formDataObject.password);
+      const hasSpecialChar = /[@$!%*?&#]/.test(formDataObject.password);
       
-      if (hospitalNameList.includes(formDataObject.name)) {
-        document.getElementById('error-message-wrap').innerHTML = `<div class="error-message">The Hospital already exists.</div>`;
+      if (formDataObject.userName == '') {
+        document.getElementById('error-message-wrap').innerHTML = `<div class="error-message">Please enter User Name.</div>`;
+      } else if (formDataObject.password == '') {
+        document.getElementById('error-message-wrap').innerHTML = `<div class="error-message">Please enter Password.</div>`;
+      } else if (formDataObject.password.length < minLength) {
+        document.getElementById('error-message-wrap').innerHTML = `<div class="error-message">Password must be at least 8 characters long.</div>`;
+      } else if (!hasUpperCase) {
+        document.getElementById('error-message-wrap').innerHTML = `<div class="error-message">Password must contain at least one uppercase letter.</div>`;
+      } else if (!hasLowerCase) {
+        document.getElementById('error-message-wrap').innerHTML = `<div class="error-message">Password must contain at least one lowercase letter.</div>`;
+      } else if (!hasNumber) {
+        document.getElementById('error-message-wrap').innerHTML = `<div class="error-message">Password must contain at least one number.</div>`;
+      } else if (!hasSpecialChar) {
+        document.getElementById('error-message-wrap').innerHTML = `<div class="error-message">Password must contain at least one special character (@, $, !, %, etc.).</div>`;
+      } else if( formDataObject.password != formDataObject.RenewPassword ) {
+        document.getElementById('error-message-wrap').innerHTML = `<div class="error-message">Paswords did not match</div>`;
       } else {
         
-        fetch('/addHospital', {
+        fetch('/addUser', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json'
           },
-          body: JSON.stringify(formDataObject)
+          body: JSON.stringify(sendingFormDataObject)
         })
         .then(response => response.text())
         .then(data => {
