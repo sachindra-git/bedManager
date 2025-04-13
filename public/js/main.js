1d8214adfe0da858d996077197cf8706a7c79c07
Glitch (glitch-hello-node) <none>
2025-04-13 04:07:27 +0000

    ⛴🚪 Checkpoint ./public/login.html:71079836/356 ./public/css/_common.css:71079836/318 ./public/js/main.js:71079836/964

--- public/js/main.js
+++ public/js/main.js
@@ -134,6 +134,19 @@ $( document ).ready(function() {
     });
   }
   
+  function forgotPasswordToggle() {
+    const forgotPW = document.querySelector('.forgot-password');
+    const forgotPWMsg = document.querySelector('.fp-message-wrap');
+    forgotPW.addEventListener("click", function() {
+        forgotPWMsg?.classList.toggle("active");
+    });
+    document.addEventListener("click", function(event) {
+      if (!event.target.closest(".fp-message-wrap") && userDropdown?.classList.contains('active')) {
+        forgotPWMsg?.classList.remove("active");
+      }
+    });
+  }
+  
   function signOut() {
     const signOut = document.querySelector('.sign-out a');
     signOut?.addEventListener("click", function(event) {
@@ -192,4 +205,5 @@ $( document ).ready(function() {
   userToggle();
   signOut();
   adminOnly();
+  forgotPasswordToggle();
 });
